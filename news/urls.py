006f82d920ddceadef1baf6294b2ad3e4dee61b8
semen-ksv <ksvsem@gmail.com>
2020-06-06 21:56:00 +0300

    implement categories side menu

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from news.views import index
+from news.views import index, get_category
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('category/<int:category_id>/', get_category),
 ]
--- news/views.py
+++ news/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-from news.models import News
+from news.models import News, Category
 
 
 def index(request):
+    # вивод всех новостей на шаблоне index
     news = News.objects.all()
     # news = News.objects.order_by('-created')
-    defaul_img = 'media/photos/Article.jpg'
-    return render(request, 'news/index_news.html', {'news': news, 'title': 'News', 'article_imp': defaul_img})
+    default_img = 'media/photos/Article.jpg'
+    categories = Category.objects.all()
+    return render(request, 'news/index_news.html', {'news': news,
+                                                    'title': 'All News',
+                                                    'article_imp': default_img,
+                                                    'categories': categories
+                                                    })
+
+
+def get_category(request, category_id):
+    # подбор новостей по категориям с бокового меню
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news,
+                                                  'category': category,
+                                                  'categories': categories
+                                                  })

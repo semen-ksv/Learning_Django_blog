4971174bf455ab56b8bec34b7f42328d19bf33b5
semen-ksv <ksvsem@gmail.com>
2020-06-09 22:18:27 +0300

    remake index function at class which inherit ListView

--- news/views.py
+++ news/views.py
@@ -1,18 +1,43 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView
+
 from news.models import News, Category
 from .forms import NewsForm
 
 
-def index(request):
-    # вивод всех новостей на шаблоне index
-    news = News.objects.all()
-    # news = News.objects.order_by('-created')
-    default_img = 'media/photos/Article.jpg'
-    # categories = Category.objects.all()
-    return render(request, 'news/index_news.html', {'news': news,
-                                                    'title': 'All News',
-                                                    'article_img': default_img,
-                                                    })
+class HemeNews(ListView):
+    """вивод всех новостей, используеться вместо функции index"""
+    model = News
+    template_name = 'news/index_news.html'
+    context_object_name = 'news'
+
+    # default_img = 'media/photos/Article.jpg'
+    # extra_context = {'title': 'All News',
+    #                  'article_img': default_img,
+    #                  }
+    def get_context_data(self, *, object_list=None, **kwargs):
+        # лучше использовать вместо extra_context
+        context = super(HemeNews, self).get_context_data(**kwargs)
+        default_img = 'media/photos/Article.jpg'
+        context['title'] = 'All News'
+        context['article_img'] = default_img
+        return context
+
+    def get_queryset(self):
+        # фильтруем вывод новостей на страницу, по галочке публикации
+        return News.objects.filter(published=True)
+
+
+# def index(request):
+#     # вивод всех новостей на шаблоне index
+#     news = News.objects.all()
+#     # news = News.objects.order_by('-created')
+#     default_img = 'media/photos/Article.jpg'
+#     # categories = Category.objects.all()
+#     return render(request, 'news/index_news.html', {'news': news,
+#                                                     'title': 'All News',
+#                                                     'article_img': default_img,
+#                                                     })
 
 
 def get_category(request, category_id):
@@ -24,10 +49,12 @@ def get_category(request, category_id):
                                                   'category': category,
                                                   })
 
+
 def view_news(request, news_id):
     # news_item = News.objects.get(pk=news_id)
     news_item = get_object_or_404(News, pk=news_id)
     return render(request, 'news/view_news.html', {'article': news_item})
+
 
 def add_news(request):
     # добавили форму связанную с данными и не связанную
